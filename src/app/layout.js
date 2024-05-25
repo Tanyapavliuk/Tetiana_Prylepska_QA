3e13d69542afad7d8da8c49baf0b6cc0b0ca5005
Tanya Pavliuk <tanyapavlyk1998@gmail.com>
2024-05-25 19:03:52 +0300

    +

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,31 +5,6 @@ import { Header } from "@/components/Header";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 
-const BASE_URL = "https://tetiana-prylepska-qa.vercel.app";
-
-export const metadata = {
-  title: "QA Tetiana Prylepska",
-  description: "Hello! It is the portfolio website for Tetiana Prilepska. I am a QA Engineer. Find out more about me.",
-  keywords: "QA, portfolio",
-  locale: "en_EN",
-  metadataBase: new URL(BASE_URL),
-  openGraph: {
-    title: "QA Tetiana Prylepska",
-    description:
-      "Hello! It is the portfolio website for Tetiana Prilepska. I am a QA Engineer. Find out more about me.",
-    url: BASE_URL,
-    type: "website",
-    images: [
-      {
-        url: "/Og.jpg",
-        width: 1200,
-        height: 630,
-        alt: "QA Tetiana Prylepska",
-      },
-    ],
-  },
-};
-
 export default function RootLayout({ children }) {
   return (
     <html className='scroll-smooth'>
--- src/app/page.js
+++ src/app/page.js
@@ -4,6 +4,31 @@ import { Hero } from "@/components/Hero";
 import { Recommendation } from "@/components/Recommendation";
 import { Sql } from "@/components/Sql";
 
+const BASE_URL = "https://tetiana-prylepska-qa.vercel.app";
+
+export const metadata = {
+  title: "QA Tetiana Prylepska",
+  description: "Hello! It is the portfolio website for Tetiana Prilepska. I am a QA Engineer. Find out more about me.",
+  keywords: "QA, portfolio",
+  locale: "en_EN",
+  metadataBase: new URL(BASE_URL),
+  openGraph: {
+    title: "QA Tetiana Prylepska",
+    description:
+      "Hello! It is the portfolio website for Tetiana Prilepska. I am a QA Engineer. Find out more about me.",
+    url: BASE_URL,
+    type: "website",
+    images: [
+      {
+        url: "/Og.jpg",
+        width: 1200,
+        height: 630,
+        alt: "QA Tetiana Prylepska",
+      },
+    ],
+  },
+};
+
 export default function Home() {
   return (
     <main>
