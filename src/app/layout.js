a4aef964bef5fed6f34b0304c3737fbf2eb7696d
Tanya Pavliuk <tanyapavlyk1998@gmail.com>
2024-05-25 10:08:30 +0300

    update import

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 import { Header } from "@/components/Header";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import meta from '@/data/metadata.js'
+import {meta} from '@/data/metadata.js'
 
 
 
