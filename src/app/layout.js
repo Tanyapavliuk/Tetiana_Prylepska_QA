e6e8ee91c4ab0864095f8d67408c9a6111981486
Tanya Pavliuk <tanyapavlyk1998@gmail.com>
2024-05-25 10:06:29 +0300

    update import metadata in Layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 import { Header } from "@/components/Header";
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import meta from '@/data/metadata.json'
+import meta from '@/data/metadata.js'
 
 
 
